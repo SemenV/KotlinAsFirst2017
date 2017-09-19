6d2749c575e0301f501ab3e27ecb297eb7717b9c
zxc <semen.vinn@mail.ru>
2017-09-19 22:10:17 +0300

    Исправлен lesson 1

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -68,7 +68,7 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = (sagenes
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(grad: Int, min: Int, sec: Int): Double = ((grad+min/60.toDouble()+sec/3600.toDouble())*PI)/180.toDouble()
+fun angleInRadian(grad: Int, min: Int, sec: Int): Double = ((grad+min/60.0+sec/3600.0)*PI)/180.0
 
 /**
  * Тривиальная
@@ -93,7 +93,8 @@ fun thirdDigit(number: Int): Int = (number%1000)/100
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = (hoursArrive*60+minutesArrive)-(hoursDepart*60+minutesDepart)
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int
+        = (hoursArrive*60+minutesArrive)-(hoursDepart*60+minutesDepart)
 
 /**
  * Простая
@@ -102,7 +103,8 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = initial*(1+percent/100.toDouble())*(1+percent/100.toDouble())*(1+percent/100.toDouble())
+fun accountInThreeYears(initial: Int, percent: Int): Double
+        = initial*(exp(log(1+percent/100.0)*3))
 
 /**
  * Простая
