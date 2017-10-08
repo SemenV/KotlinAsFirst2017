d7d042cbb9990aa3b32b4e20db77a9e5f2c0e274
zxc <semen.vinn@mail.ru>
2017-10-09 01:03:43 +0300

    lesson5

--- src/lesson5/task1/Parse.kt
+++ src/lesson5/task1/Parse.kt
@@ -13,16 +13,11 @@ import java.util.*
 fun timeStrToSeconds(str: String): Int {
     val parts = str.split(":")
     var result = 0
-    try {
         for (part in parts) {
             val number = part.toInt()
             result = result * 60 + number
         }
         return result
-    }
-    catch(e: NumberFormatException){
-        return -1
-    }
 }
 
 /**
